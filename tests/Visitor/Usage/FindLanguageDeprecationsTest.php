249ba3d601c790164003381322f5d9c6c857b86a
MarvinKlemp <mail@marvin-klemp.de>
2017-04-08 12:07:53 +0200

    Fixed lang-deprecation

--- tests/Visitor/Usage/FindLanguageDeprecationsTest.php
+++ tests/Visitor/Usage/FindLanguageDeprecationsTest.php
@@ -8,30 +8,6 @@ use SensioLabs\DeprecationDetector\Visitor\Usage\FindLanguageDeprecations;
 
 class FindLanguageDeprecationsTest extends FindTestCase
 {
-    // http://php.net/manual/en/migration53.deprecated.php
-    public function testAssignOfNewByReference()
-    {
-        $source = <<<'EOC'
-<?php
-
-$foo =& new Foo();
-
-EOC;
-        $splFileInfo = $this->prophesize('Symfony\Component\Finder\SplFileInfo');
-        $phpFileInfo = $this->parsePhpFileFromStringAndTraverseWithVisitor(
-            $file = PhpFileInfo::create($splFileInfo->reveal()),
-            $source,
-            new FindLanguageDeprecations()
-        );
-
-        $this->assertEquals(
-            array(
-                new DeprecatedLanguageUsage('Assigning the return value of new by reference is now deprecated.', 'Since PHP 5.3 use normal assignment instead.', 3),
-            ),
-            $phpFileInfo->getDeprecatedLanguageUsages()
-        );
-    }
-
     public function testAssignByReference()
     {
         $source = <<<'EOC'
